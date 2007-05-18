f635a450213aa8baf4bd062d4a22684a2b7cf73b
Moyez Dharsee <mdharsee>
2007-05-18 23:25:50 +0000

    Set profile proteinName to the value of the Gene Name field in the peptide file. Removed obsolete method readPeptidesAsProfilesNewFormat.

--- src/org/baderlab/csplugins/brain/PeptideToProfileReader.java
+++ src/org/baderlab/csplugins/brain/PeptideToProfileReader.java
@@ -115,7 +115,6 @@ public class PeptideToProfileReader {
                 if (codonBiasFile != null) {
                     profile.reWeightByCodonBias(codonBiasFile);
                 }                 
-                //else profile.reWeightByNNKCodonBias();
                 profile.setProteinReference(proteinReference);
                 profile.setDomainReference(peptideList.getDomainXref());
                 profile.setDomainNumber(peptideList.getDomainNumber());
@@ -124,69 +123,7 @@ public class PeptideToProfileReader {
                 profile.setDomainSequenceStart(peptideList.getDomainRangeStart());
                 profile.setDomainSequenceStop(peptideList.getDomainRangeStop());
                 profile.setComment(peptideList.getComment());
-                profileList.add(profile);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (BioException e) {
-            e.printStackTrace();
-        }
-
-        return (profileList);
-    }
-
-        /**
-     * Reads a profile file from disk - either a single profile or a project file
-     *
-     * @param profileLength The length of the profiles to return
-     * @param terminus      Which end of the profile to return if length is less than the profile length
-     * @return A List of profiles
-     */
-    public static List readPeptidesAsProfilesNewFormat(File projectFile, int profileLength, ProteinTerminus terminus, double fuzzFactor, File codonBiasFile, boolean uniquePeptides) {
-        ProfileSearchProjectFile profileProjectFile = new ProfileSearchProjectFile();
-        BindingPeptideList peptideList;
-        ProteinProfile profile;
-        DatabaseReference proteinReference;
-        ArrayList profileList = new ArrayList();
-
-        if (projectFile == null) {
-            throw new RuntimeException("Project file must be specified.");
-        }
-
-        //test the file to see what type it is: profile or project?
-        Iterator files = null;
-        try {
-            if (profileProjectFile.isProjectFile(projectFile.toString())) {
-                //read project file
-                profileProjectFile.read(projectFile.toString());
-                files = profileProjectFile.getFileNames();
-            } else {
-                //read a single profile (list of peptides)
-                ArrayList fileList = new ArrayList(1);
-                fileList.add(projectFile.toString());
-                files = fileList.iterator();
-            }
-            while (files.hasNext()) {
-                String peptideListFileName = (String) files.next();
-                peptideList = new BindingPeptideList();
-                System.out.println("Loading peptide sequences from " + peptideListFileName);
-                peptideList.read(peptideListFileName);
-                proteinReference = peptideList.getProteinXref();
-                String profileName = new String(peptideList.getProteinName() + "-" + Integer.toString(peptideList.getDomainNumber()));
-                if (profileLength > 0) {
-                    profile = new ProteinProfile(peptideList.getSequenceIteratorByLength(profileLength, terminus, false, uniquePeptides), fuzzFactor, profileName);
-                } else {
-                    profile = new ProteinProfile(peptideList.getSequenceIterator(false, uniquePeptides), fuzzFactor, profileName);
-                }
-                if (codonBiasFile != null) {
-                    profile.reWeightByCodonBias(codonBiasFile);
-                }
-                //else profile.reWeightByNNKCodonBias();
-                profile.setProteinReference(proteinReference);
-                profile.setDomainReference(peptideList.getDomainXref());
-                profile.setDomainNumber(peptideList.getDomainNumber());
-                profile.setDomainSequence(peptideList.getDomainSequence());
-                profile.setExperimentalMethod(peptideList.getExperimentalMethod());
+                profile.setProteinName(peptideList.getProteinName());
                 profileList.add(profile);
             }
         } catch (IOException e) {
